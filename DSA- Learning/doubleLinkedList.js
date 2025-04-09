ec1ddf6bace06f13179f8d3268214f3b7f993d02
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-04-09 23:00:22 +0100

    feat: doubleLinkedList shift method

--- DSA- Learning/doubleLinkedList.js
+++ DSA- Learning/doubleLinkedList.js
@@ -18,7 +18,7 @@ class DoublyLinkedList {
   push(val) {
     const newNode = new Node(val);
 
-    if (!this.head) {
+    if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -27,6 +27,48 @@ class DoublyLinkedList {
       this.tail = newNode;
     }
     this.length++;
-    return this
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+    const currentHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.head = currentHead.next;
+    this.head.prev = null;
+    currentHead.next = null;
+    this.length--;
+    return currentHead;
   }
 }
+
+let list = new DoublyLinkedList();
+
+list.push(99);
+list.push(100);
+list.push(200);
+
+console.log(list);
+console.log(list.pop());
+console.log(list.shift());
