af6d3a2544a17e44725937a51e7650165483384d
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-04-11 17:58:34 +0100

    feat: insert method for doubleLinkedList

--- DSA- Learning/doubleLinkedList.js
+++ DSA- Learning/doubleLinkedList.js
@@ -16,8 +16,7 @@ class DoublyLinkedList {
   }
 
   push(val) {
-    const newNode = new Node(val);
-
+    var newNode = new Node(val);
     if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
@@ -29,11 +28,9 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
-
   pop() {
     if (!this.head) return undefined;
-
-    let poppedNode = this.tail;
+    var poppedNode = this.tail;
     if (this.length === 1) {
       this.head = null;
       this.tail = null;
@@ -42,33 +39,28 @@ class DoublyLinkedList {
       this.tail.next = null;
       poppedNode.prev = null;
     }
-
     this.length--;
     return poppedNode;
   }
-
   shift() {
     if (this.length === 0) return undefined;
-    const currentHead = this.head;
-
+    var oldHead = this.head;
     if (this.length === 1) {
       this.head = null;
       this.tail = null;
     } else {
-      this.head = currentHead.next;
+      this.head = oldHead.next;
       this.head.prev = null;
-      currentHead.next = null;
+      oldHead.next = null;
     }
-
     this.length--;
-    return currentHead;
+    return oldHead;
   }
-
   unshift(val) {
-    const newNode = new Node(val);
+    var newNode = new Node(val);
     if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
       this.head.prev = newNode;
       newNode.next = this.head;
@@ -77,23 +69,20 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
-
   get(index) {
     if (index < 0 || index >= this.length) return null;
     var count, current;
     if (index <= this.length / 2) {
       count = 0;
       current = this.head;
-
-      while (index !== count) {
+      while (count !== index) {
         current = current.next;
         count++;
       }
     } else {
       count = this.length - 1;
       current = this.tail;
-
-      while (index !== count) {
+      while (count !== index) {
         current = current.prev;
         count--;
       }
@@ -101,10 +90,28 @@ class DoublyLinkedList {
     return current;
   }
   set(index, val) {
-    let result = this.get(index);
-    if (!result) return false;
+    var foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 
-    result.val = val;
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    var newNode = new Node(val);
+    var beforeNode = this.get(index - 1);
+    var afterNode = beforeNode.next;
+
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode
+    this.length++;
     return true;
   }
 }
