ad86b16f97c883f6839aa564ca4169e1d7b03a3a
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-04-18 03:23:31 +0100

    Feat:BinarySearchTree insert method

--- DSA- Learning/binarySearchTree.js
+++ DSA- Learning/binarySearchTree.js
@@ -10,9 +10,47 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    var newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+
+      while (true) {
+        if (val === current.value) return undefined;
+        if (val < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
-
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(16);
+tree.insert(7);
+tree.insert(2);
+tree.insert(3);
+console.log(tree);
 
 // Pain in the ass way of creating a tree manually
 // var tree = new BinarySearchTree();
