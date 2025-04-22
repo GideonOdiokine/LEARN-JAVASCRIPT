9d198a763689ff121502efc4619c6cd7fed5749c
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-04-22 04:38:28 +0100

    Feat: Breadth_First_Tree Method

--- DSA- Learning/binarySearchTree.js
+++ DSA- Learning/binarySearchTree.js
@@ -52,18 +52,43 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  BFS() {
+    var node = this.root;
+    var data = [];
+    var queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
+
+//      10
+//   6    15
+// 3  8     20
 
 var tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(16);
-tree.insert(7);
-tree.insert(2);
+tree.insert(6);
+tree.insert(15);
 tree.insert(3);
-console.log(tree.find(11));
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.BFS());
+// tree.insert(13);
+// tree.insert(11);
+// tree.insert(16);
+// tree.insert(7);
+// tree.insert(2);
+// tree.insert(3);
+// console.log(tree.find(11));
 
 // Pain in the ass way of creating a tree manually
 // var tree = new BinarySearchTree();
